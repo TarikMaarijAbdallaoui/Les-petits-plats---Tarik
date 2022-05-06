d67a75bbd0db18442750794bcf8dff967d9eb52d
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-05-06 18:33:41 +0200

    Modifier le style des plats

--- scripts/factories/plats.js
+++ scripts/factories/plats.js
@@ -1,57 +1,56 @@
-function getIngredients (ingredientsList) {
-        let list = "";
-        ingredientsList.forEach((item) => {
-                list += `<li><strong>${item.ingredient}</strong>${
-                        item.quantity || item.quantite
-                        ? ": " + (item.quantity || item.quantite)
-                        : ""
-                } ${item.unit ? item.unit : ""}</li>`;
-        });
+function getIngredients(ingredientsList) {
+  let list = "";
+  ingredientsList.forEach((item) => {
+    list += `<li><strong>${item.ingredient}</strong>${
+      item.quantity || item.quantite
+        ? ": " + (item.quantity || item.quantite)
+        : ""
+    } ${item.unit ? item.unit : ""}</li>`;
+  });
 
-        return list;
+  return list;
 }
 
+export function showPlats(platsList) {
+  try {
+    platsList.forEach((plat) => {
+      const recipeSection = document.querySelector("#plats");
 
-export function showPlats(platsList){
-        try {
-                platsList.forEach((plat) => {
-                        const recipeSection = document.querySelector("#plats");
+      const platCard =
+ `<article class="single-plat">
 
-                        const platCard =
-                        `<article class="single-plat">
+        <div class="plat-image">
+           <img src="" alt="">     
+        </div>   
 
-                             <div class="plat-image">
-                                <img src="" alt="">     
-                             </div>   
+        <div class="plat-content">
 
-                             <div class="plat-content">
+           <div class="plat-header">
+                <div class="title">
+                   <h3>${plat.name}</h3>
+                </div>
+                <div class="time">
+                   <i class="fa-regular fa-clock"></i>
+                   <span>${plat.time} min</span>
+                </div>
+           </div>
 
-                                <div class="plat-header">
-                                     <div class="title">
-                                        <h3>${plat.name}</h3>
-                                     </div>
-                                     <div class="time">
-                                        <i class="fa-regular fa-clock"></i>
-                                        <span>${plat.time} min</span>
-                                     </div>
-                                </div>
+           <div class="plat-body">
+              <div class="ingredients">
+                <ul>
+                   ${getIngredients(plat.ingredients)}
+                </ul>
+              </div>
+              <div class="description">
+                <span>${plat.description}</span>
+              </div>
+           </div>
+        </div>    
+ </article>`;
 
-                                <div class="plat-body">
-                                     <div class="ingredients">
-                                        <ul>
-                                            ${getIngredients(plat.ingredients)}
-                                        </ul>
-                                      </div>
-                                      <div class="description">
-                                        <span>${plat.description}</span>
-                                      </div>
-                                </div>
-                             </div>    
-                         </article>`;
-
-                         recipeSection.insertAdjacentHTML("beforeend", platCard);
-                });
-        } catch (error) {
-                console.error();
-        }
+      recipeSection.insertAdjacentHTML("beforeend", platCard);
+    });
+  } catch (error) {
+    console.error();
+  }
 }
