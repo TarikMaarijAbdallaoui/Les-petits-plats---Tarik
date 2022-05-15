4838280d9bb32ca82e2d9237f1cf4e7d0be40ced
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-05-15 21:42:11 +0200

    création des constantes searchBar et recipeSection + la fonction searchBar Results(recipes)

--- scripts/index.js
+++ scripts/index.js
@@ -25,10 +25,72 @@ function loadFilters(recipes) {
           ustensilsBlock.innerHTML += `<li role="option" class="ustensils-option">${utensils}</li>`;
         });
       
+};
+
+
+const searchBar = document.getElementById("chercher");
+const recipeSection = document.getElementById("plats");
+
+/**
+  Barre de recherche principale, recherche de correspondances
+  d'abord dans les titres puis dans la description
+ */
+ function searchBarResults(recipes) {
+        searchBar.addEventListener("keyup", function (e) {
+          /* Normalisez le texte écrit afin qu'il ne distingue pas les accents ou
+          caractères spéciaux */
+          const inputValue = normalizeString(e.target.value);
+      
+          // Si el texto ingresado en la barra de busqueda tiene 3 o mas caracteres
+          // se ejecuta la búsqueda primero en los ingredientes y luego en la descripcion
+          if (inputValue.length > 2) {
+            const matchedRecipes = recipes.filter((recipe) => {
+              return (
+                // el metodo includes() verifica si el texto del campo de busqueda
+                // se encuentra los ingredientes o la descripcion de la receta
+                normalizeString(recipe.name).includes(inputValue) ||
+                recipe.ingredients.some((i) =>
+                  normalizeString(i.ingredient).includes(inputValue)
+                ) ||
+                normalizeString(recipe.description).includes(inputValue)
+              );
+            });
+      
+            // se limpia la seccion de platos para renderizar nuevamente los
+            // resultados de la busqueda
+            recipeSection.innerHTML = "";
+      
+            // si no hay resultados de busqueda se muestra una carita triste
+            if (matchedRecipes.length === 0) {
+              recipeSection.innerHTML = `<div id="nomatch">
+              <img src="./medias/sad-face-gray.svg" alt="" />
+              <p>Cette recherche n'a renvoyé aucune correspondance.</p>
+            </div>`;
+              document.querySelector(".matchs").remove();
+            }
+            // de lo contrario se muestran los platos que coinciden con la
+            // busqueda
+            else {
+              showPlats(matchedRecipes);
+              //matchMessage(matchedRecipes);
+            }
+          }
+          // si lo que se escribió no tiene al menos 3 caracteres entonces
+          // se muestran todos los platos
+          else {
+            //document.querySelector(".matchs").remove();
+            recipeSection.innerHTML = "";
+            showPlats(recipes);
+          }
+        });
       }
       
-
+/* la fonction d'entrée à l'application */
       window.addEventListener("load", (e) => {
         loadFilters(recipes);
         showPlats(recipes);
-});      
+        searchBarResults(recipes);
+});    
+
+
+/*  */
