2af9e91cbbb8e4dd83880e491adc04ad42ff4fd8
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-05-15 20:40:59 +0200

    importer la fonction tous les filtres + créer une fonction loadfilter

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,33 @@
 import {recipes} from "../data/recipes.js";
 import { showPlats } from "./factories/plats.js";
+import {allFilters} from "./utils/filters.js";
 
-window.addEventListener("load", (e) => {
+
+/* importation elements des filtres*/
+
+function loadFilters(recipes) {
+        const { allIngredients, allAppareils, allUstensils } = allFilters(recipes);
+        const ingredientsBlock = document.querySelector("#ingredients-list");
+        const appareilsBlock = document.querySelector("#appareils-list");
+        const ustensilsBlock = document.querySelector("#ustensils-list");
+        
+      
+        allIngredients.forEach((ingredient) => {
+          ingredientsBlock.innerHTML += `<li role="option" class="ingredients-option">${ingredient}</li>`;
+        });
+      
+        allAppareils.forEach((appareil) => {
+          appareilsBlock.innerHTML += `<li role="option" class="appareils-option">${appareil}</li>`;
+        });
+
+        allUstensils.forEach((utensils) => {
+          ustensilsBlock.innerHTML += `<li role="option" class="ustensils-option">${utensils}</li>`;
+        });
+      
+      }
+      
+
+      window.addEventListener("load", (e) => {
+        loadFilters(recipes);
         showPlats(recipes);
-});
+});      
