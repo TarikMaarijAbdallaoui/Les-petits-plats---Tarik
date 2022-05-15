ac5f7a36303a85af847f080d790cc5bad738f70e
tarikopenclassrooms <maarijtarik@hotmail.com>
2022-05-15 20:34:04 +0200

    Intégration des éléments aux 3 filtres

---
+++ scripts/utils/filters.js
@@ -0,0 +1,26 @@
+export const allFilters = (recipes) => {
+        let allIngredients = [];
+        let allAppareils = [];
+        let allUstensils = [];
+      
+        recipes.forEach((recipe) => {
+          allIngredients = [
+            ...new Set([
+              ...allIngredients,
+              ...recipe.ingredients.map((i) => i.ingredient),
+            ]),
+          ];
+         
+          allAppareils = [...new Set([...allAppareils, ...[recipe.appliance]])];
+
+          allUstensils = [
+            ...new Set([...allUstensils, ...recipe.ustensils.map((u) => u)])];
+        });
+      
+        allIngredients = allIngredients.sort();
+        allAppareils = allAppareils.sort();
+        allUstensils = allUstensils.sort();
+      
+        return { allIngredients, allAppareils, allUstensils };
+      };
+      
